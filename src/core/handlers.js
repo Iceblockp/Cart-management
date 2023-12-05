ce6ca139c37add4ea26fe85db0f1f47e37c3b0ba
Iceblockp <phonyo126@gmail.com>
2023-12-05 11:25:01 +0630

    handlerFinish

--- src/core/handlers.js
+++ src/core/handlers.js
@@ -7,6 +7,8 @@ export const searchBtnHandler = () => {
     searchInput.classList.toggle("opacity-0");
     searchInput.classList.toggle("pointer-events-none");
     searchInput.focus();
+
+
 }
 
 export const searchInputHandler = (event) => {
